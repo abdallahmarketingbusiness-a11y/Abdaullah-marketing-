083958fff8fbe46ece16f003fb207705b0c32367
abdallahmarketingbusiness-a11y <abdallahmarketing.business@gmail.com>
2026-07-31 10:34:08 +0300

    seo

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://abdaullah-marketing-3dmf.vercel.app/sitemap.xml',
+  };
+}
